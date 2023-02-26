37ad173946c6b6869cef16a861d1f21ed621f315
Sovichea Tep <sovichea.tep@gmail.com>
2023-02-26 23:29:45 +0700

    modify diagnostic sign

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -48,3 +48,9 @@ require("lspconfig").pyright.setup {
     on_attach = on_attach,
     capabilities = capabilities
 }
+
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -75,6 +75,18 @@ local plugins = {
     {'romgrk/barbar.nvim', dependencies = 'nvim-tree/nvim-web-devicons'},
 
     { "lukas-reineke/indent-blankline.nvim" },
+
+    {
+        "folke/trouble.nvim",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            }
+        end
+    },
 }
 
 
